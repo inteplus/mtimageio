00b557fc8e32c3cf212e1c37ab874c4baa4357d3
Minh-Tri Pham <pmtri80@gmail.com>
2022-12-18 07:58:57 +0000

    updating setup.py

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
 #!/usr/bin/env python3
 
 from setuptools import setup, find_namespace_packages
-from mt.opencv.version import version
+from mt.imageio.version import version
 
 setup(
-    name="mtopencv",
+    name="mtimageio",
     version=version,
-    description="Minh-Tri Pham's extra modules using OpenCV",
+    description="Minh-Tri Pham's extra modules using imageio",
     author=["Minh-Tri Pham"],
     packages=find_namespace_packages(include=["mt.*"]),
-    scripts=[
-        "scripts/immview",
-    ],
+    # scripts=[
+    #    "scripts/immview",
+    # ],
     install_requires=[
         # 'h5py>=3', # for pdh5 file format. Lazy import because TX2 may not need it.
-        # 'opencv-python', # let them install opencv-python or opencv-python-headless or whatever
-        "ansicolors",  # for displaying images on the terminal
-        "pyturbojpeg",  # for encoding/decoding images
+        "imageio>=2.15",  # for loading image files in a modern way
         "mtbase>=3.1",  # to have mt.np.dequantise_images
-        "mtgeo>=0.10",
+        "mtgeo>=1.8",  # to have the latest mt.opencv.image.Image implementation
     ],
 )
