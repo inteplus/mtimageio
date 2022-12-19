ca47a9815c1f9d1a409e477d39b8f9a2cd04391b
Minh-Tri Pham <pmtri80@gmail.com>
2022-12-19 10:45:40 +0000

    v0.1: moved mtopencv's script immview to here

--- setup.py
+++ setup.py
@@ -9,14 +9,14 @@ setup(
     description="Minh-Tri Pham's extra modules using imageio",
     author=["Minh-Tri Pham"],
     packages=find_namespace_packages(include=["mt.*"]),
-    # scripts=[
-    #    "scripts/immview",
-    # ],
+    scripts=[
+        "scripts/immview",
+    ],
     install_requires=[
         # 'h5py>=3', # for pdh5 file format. Lazy import because TX2 may not need it.
         "Pillow>=9.0",  # for processing PNG images
         "imageio>=2.15",  # for loading image files in a modern way
         "mtbase>=3.1",  # to have mt.np.dequantise_images
-        "mtopencv>=1.8",  # to have the latest mt.opencv.image.Image implementation
+        "mtopencv>=1.9",  # to have mtopencv's immview script removed
     ],
 )
