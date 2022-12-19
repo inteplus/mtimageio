4902c43fc6a08556d9aae442d260036147c53efd
Minh-Tri Pham <pmtri80@gmail.com>
2022-12-19 08:59:47 +0000

    adding imread module

--- mt/iio/__init__.py
+++ mt/iio/__init__.py
@@ -1,9 +1,4 @@
 from imageio import __version__
 from imageio.v3 import *
 
-# from mt.opencv import cv2, logger
-# from mt.opencv.polygon import *
-# from mt.opencv.warping import *
-# from mt.opencv.image import *
-# from mt.opencv.imgcrop import *
-# from mt.opencv.ansi import *
+from mt.imageio import *
